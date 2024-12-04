8d96f5601cf7bf793167496190b318337c75c598
Асылгареев Вадим <710271119@mail.ru>
2024-12-05 00:15:13 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, Query, Body
+import uvicorn
+
+
+app = FastAPI()
+
+hotels = [{"id": 1, "title": "Sochi"}, {"id": 2, "title": "Dubai"}]
+
+
+@app.get("/hotels")
+def hotel_get(
+    id: int | None = Query(default=None, description="ID отеля"),
+    title: str | None = Query(default=None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.delete("/hotels/{hotel_id}")
+def hotel_delete(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+
+@app.post("/hotels")
+def hotel_post(title: str = Body(embed=True)):
+    global hotels
+    hotel_id = len(hotels) + 1
+    hotels.append({
+        "id": hotel_id,
+        "title": title}
+
+    )
+    return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
