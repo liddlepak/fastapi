75808ef2e5bbc58b8edf229276fb5e7797aaec07
Асылгареев Вадим <710271119@mail.ru>
2025-01-14 17:50:08 +0500

    change get_free_hotels func

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -11,22 +11,6 @@ class HotelsRepositories(BaseRepositories):
     model = HotelModel
     schema = Hotel
 
-    async def get_filtred(self, *filter, title, location, limit, offset):
-        query = select(HotelModel).filter(*filter)
-        if title:
-            query = query.filter(
-                func.lower(HotelModel.title).contains(title.lstrip().lower())
-            )
-        if location:
-            query = query.filter(
-                func.lower(
-                    HotelModel.location).contains(location.lstrip().lower())
-            )
-        query = query.limit(limit).offset(offset)
-        result = await self.session.execute(query)
-        return [Hotel.model_validate(
-            model, from_attributes=True) for model in result.scalars().all()]
-
     async def get_free_hotels(
             self, date_from, date_to, title, limit, offset, location):
         free_ids_rooms = get_free_rooms(date_from=date_from, date_to=date_to)
@@ -34,9 +18,12 @@ class HotelsRepositories(BaseRepositories):
             select(RoomsModel.hotel_id).
             select_from(RoomsModel).
             filter(RoomsModel.id.in_(free_ids_rooms)))
-        return await self.get_filtred(
-            HotelModel.id.in_(free_hotels_ids),
-            title=title,
-            location=location,
-            limit=limit,
-            offset=offset)
+        if title:
+            free_hotels_ids = free_hotels_ids.filter(
+                func.lower(HotelModel.title).contains(title.lstrip().lower()))
+        if location:
+            free_hotels_ids = free_hotels_ids.filter(
+                func.lower(
+                    HotelModel.location).contains(location.lstrip().lower()))
+        free_hotels_ids = free_hotels_ids.limit(limit).offset(offset)
+        return await self.get_filtred(HotelModel.id.in_(free_hotels_ids))
